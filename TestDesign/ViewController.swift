7f661b1efd1db281f349d385768338a3c171bab5
Ivan Chernetsky <inch70798@gmail.com>
2020-02-29 21:43:18 +0200

    Initial Commit

---
+++ TestDesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDesign
+//
+//  Created by Ivan Chernetskiy on 29.02.2020.
+//  Copyright © 2020 Ivan Chernetskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
